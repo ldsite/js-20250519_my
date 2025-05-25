d1e99f9e3e36c1e816fef351fc879917bd428619
ldsite <147878620+ldsite@users.noreply.github.com>
2025-05-25 12:57:29 +0300

    add function sotr array

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    const sorted = [...arr];
+    return sorted.sort(function (a, b) {
+        let result = a.localeCompare(b, ['ru', 'en'], { sensitivity: 'base' });
+        if (result === 0) {
+          if (a < b) return -1;
+          if (a > b) return 1;
+          return 0;
+        }
+        return param === 'asc' ? result : -result;
+    });
 }

228d3a5c969e674a904c305bc794761381cf52a1
ldsite <147878620+ldsite@users.noreply.github.com>
2025-05-29 10:14:51 +0300

    add task3 - 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,12 +7,12 @@
 export function sortStrings(arr, param = 'asc') {
     const sorted = [...arr];
     return sorted.sort(function (a, b) {
-        let result = a.localeCompare(b, ['ru', 'en'], { sensitivity: 'base' });
+        let result = a.localeCompare(b, ['ru', 'en'], { sensitivity: 'base'});
         if (result === 0) {
           if (a < b) return -1;
           if (a > b) return 1;
           return 0;
         }
-        return param === 'asc' ? result : -result;
+       return param === 'asc' ? result : -result;
     });
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+ const obj1 = {};
+    for( const key in obj){
+        obj1[obj[key]]=key;
+    }
+    return obj?obj1:undefined;
 }

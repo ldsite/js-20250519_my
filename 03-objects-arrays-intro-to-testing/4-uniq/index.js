e6706a4bd92a85d6865363cc7756229909ea4e55
ldsite <147878620+ldsite@users.noreply.github.com>
2025-05-29 21:46:58 +0300

    add #03 unic function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -1,8 +1,18 @@
+
 /**
  * uniq - returns array of uniq values:
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+    if (Array.isArray(arr)) {
+        if (arr.length > 0) {
+            return Array.from(new Set(arr));
+        } else {
+            return [];
+        }
+    }else {
+        return [];
+    }
 
 }

f911541e589d954880545b7720b7c2a5cccaf1a3
ldsite <147878620+ldsite@users.noreply.github.com>
2025-06-15 12:41:28 +0300

    add #5 NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,53 @@
 export default class NotificationMessage {
+    element;
 
+    static lastShownComponent;
+    constructor(message = '', {
+        type = '',
+        duration = 1000
+    } = {}) {
+        this.type = type;
+        this.duration = duration;
+        this.message = message;
+        this.element = this.createElement(this.createTemplate());
+    }
+    remove() {
+        if (this.element) {   
+            this.element.remove();
+            this.element = null;
+        }
+    }
+    show(targetElement = document.body) {
+        if (NotificationMessage.lastShownComponent) {
+            NotificationMessage.lastShownComponent.remove();
+        }
+        this.element = this.createElement(this.createTemplate());
+        targetElement.appendChild(this.element);
+        NotificationMessage.lastShownComponent = this;
+        if (this.duration > 0) {
+            this.timerId = setTimeout(() => this.remove(), this.duration);
+        }
+    }
+    
+    createElement(template) {
+        const element = document.createElement('div');
+        element.innerHTML = template.trim();
+        return element.firstElementChild;
+    }
+    createTemplate() {
+        return (`
+                <div class="notification ${this.type}" style="--value:20s">
+                    <div class="timer"></div>
+                    <div class="inner-wrapper">
+                      <div class="notification-header">success</div>
+                      <div class="notification-body">
+                        ${this.message}
+                      </div>
+                    </div>
+                  </div>
+                `);
+    }
+    destroy() {
+        this.remove();
+    }
 }
